8a712ca6382f7526125efa1e9a70654e8ac9e84e
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-02-01 11:40:51 -0500

    Add array row/column numbers to board (not algebraic notation). Game class comment out to force run

--- lib/game.rb
+++ lib/game.rb
@@ -3,12 +3,13 @@
 # lib/chess_spec.rb
 
 class Game
-  def initialize(board = Board.new, player_one = Player.new('white'), player_two = Player.new('black'))
+  def initialize(board = Board.new)#, player_one = Player.new('white'), player_two = Player.new('black'))
     @board = board
-    @player_one = player_one
-    @player_two = player_two
+    #@player_one = player_one
+    #@player_two = player_two
   end
 
   def start
+    p @board.display
   end
 end
