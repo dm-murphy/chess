0ead6e12b0cc2b541dcbb961a0120fc58ec447d5
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-02-02 20:52:12 -0500

    Add start_turn script and current_player instance variable

--- lib/game.rb
+++ lib/game.rb
@@ -7,19 +7,27 @@ class Game
     @board = board
     @player_one = player_one
     @player_two = player_two
+    @current_player = @player_one
   end
 
   def start
     @board.start_pieces_knight
+    start_turn
+  end
+
+  def start_turn
     p @board.display
+    @current_player.prompt_piece
   end
 end
 
 # Next Pseudo Steps
     
-    # Initiliaze new Player One object in Game class
-    # Set Player class values for piece color, name ("Player One vs Player Two")
+    
+    
     # Make input prompt from Game to Player object
+      # Makje current player
+      # Prompt current player for piece
     # Make Player class method to receive user input on piece selection
     # Prevent non-eligible selections
     # Show possible moves
