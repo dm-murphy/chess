1bc88a05b35c18af8fe37a2aac7cb153a676d5ba
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-02-03 12:14:21 -0500

    Set test knight to node object instead of display value and change the HEREDOC to display value

--- lib/board.rb
+++ lib/board.rb
@@ -16,7 +16,7 @@ class Board
     3    #{@grid[3][0]}    #{@grid[3][1]}    #{@grid[3][2]}    #{@grid[3][3]}    #{@grid[3][4]}    #{@grid[3][5]}    #{@grid[3][6]}    #{@grid[3][7]}
     2    #{@grid[2][0]}    #{@grid[2][1]}    #{@grid[2][2]}    #{@grid[2][3]}    #{@grid[2][4]}    #{@grid[2][5]}    #{@grid[2][6]}    #{@grid[2][7]}
     1    #{@grid[1][0]}    #{@grid[1][1]}    #{@grid[1][2]}    #{@grid[1][3]}    #{@grid[1][4]}    #{@grid[1][5]}    #{@grid[1][6]}    #{@grid[1][7]}
-    0    #{@grid[0][0]}    #{@grid[0][1]}    #{@grid[0][2]}    #{@grid[0][3]}    #{@grid[0][4]}    #{@grid[0][5]}    #{@grid[0][6]}    #{@grid[0][7]}
+    0    #{@grid[0][0]}    #{@grid[0][1].display}    #{@grid[0][2]}    #{@grid[0][3]}    #{@grid[0][4]}    #{@grid[0][5]}    #{@grid[0][6]}    #{@grid[0][7]}
          0    1    2    3    4    5    6    7
     
     HEREDOC
@@ -25,7 +25,7 @@ class Board
 
   def start_pieces_knight
     # Display test method to start a white knight piece in correct position
-    @grid[0][1] = Knight.new([0, 1], "white").display
+    @grid[0][1] = Knight.new([0, 1], "white")
   end
   
 end
