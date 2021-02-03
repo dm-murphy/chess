27dbaf7d8eb74d5cdbb201f3123e87f625298756
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-02-02 20:56:52 -0500

    Add #prompt_piece

--- lib/player.rb
+++ lib/player.rb
@@ -3,9 +3,12 @@
 class Player
   attr_accessor :name, :pieces
 
-  def initialize (name, pieces)
+  def initialize(name, pieces)
     @name = name
     @pieces = pieces
   end
-
+  
+  def prompt_piece
+    gets.chomp
+  end
 end
