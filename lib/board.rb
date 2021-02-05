f302c2d9fccf662863fc45591ed64a5607f33eb7
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-02-04 21:46:27 -0500

    #change_piece moves node object on Board's grid

--- lib/board.rb
+++ lib/board.rb
@@ -16,7 +16,7 @@ class Board
     3    #{@grid[3][0]}    #{@grid[3][1]}    #{@grid[3][2]}    #{@grid[3][3]}    #{@grid[3][4]}    #{@grid[3][5]}    #{@grid[3][6]}    #{@grid[3][7]}
     2    #{@grid[2][0]}    #{@grid[2][1]}    #{@grid[2][2]}    #{@grid[2][3]}    #{@grid[2][4]}    #{@grid[2][5]}    #{@grid[2][6]}    #{@grid[2][7]}
     1    #{@grid[1][0]}    #{@grid[1][1]}    #{@grid[1][2]}    #{@grid[1][3]}    #{@grid[1][4]}    #{@grid[1][5]}    #{@grid[1][6]}    #{@grid[1][7]}
-    0    #{@grid[0][0]}    #{@grid[0][1].display}    #{@grid[0][2]}    #{@grid[0][3]}    #{@grid[0][4]}    #{@grid[0][5]}    #{@grid[0][6]}    #{@grid[0][7]}
+    0    #{@grid[0][0]}    #{@grid[0][1]}    #{@grid[0][2]}    #{@grid[0][3]}    #{@grid[0][4]}    #{@grid[0][5]}    #{@grid[0][6]}    #{@grid[0][7]}
          0    1    2    3    4    5    6    7
     
     HEREDOC
@@ -25,7 +25,12 @@ class Board
 
   def start_pieces_knight
     # Display test method to start a white knight piece in correct position
+    
+    # This seems wrong
     @grid[0][1] = Knight.new([0, 1], "white")
+
+    # Would all knight objects just be made at once?
+    # How do I access just this one object then? 
   end
   
   # So in board class, I'm making all the white and black pieces as nodes right from the start
@@ -33,10 +38,16 @@ class Board
   # Can the board object coordinates work instead? How do I take that knight object node and reassign its coordinates?
 
   def change_piece(row, column, end_row, end_column)
-    @grid[row][column].coord = @grid[end_row][end_column]
+    # @grid[row][column].coord = @grid[end_row][end_column]
     #@grid[row][column].coord = [end_row][end_column]
-
-    p @grid[end_row][end_column]
-    p @grid[row][column]
+    puts "This is @grid[row][column]: #{@grid[row][column]}"
+    puts "This is @grid[end_row][end_column]: #{@grid[end_row][end_column]}"
+    temp_node = @grid[row][column]
+    p temp_node
+    @grid[end_row][end_column] = temp_node
+    @grid[row][column] = "-"
+    p @grid[0][0]
+    puts "This is the new @grid[end_row][end_column]: #{@grid[end_row][end_column]}"
+    puts "This is the new @grid[row][column]: #{@grid[row][column]}"
   end
 end
