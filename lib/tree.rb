4bd78a8d6cf717e56659a66f5ac71b4e6d799b5b
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-02-04 21:42:32 -0500

    Commented out Tree class for now. Using Board's 2D array to hold nodes for now

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,42 +2,42 @@
 
 # Creates tree object or "board", holds Knight class node objects, finds shortest moves from start to finish
 class Tree
-  attr_accessor :start, :finish
+  # attr_accessor :start, :finish
 
-  def knight_moves(start, finish)
-    @start = Knight.new(start)
-    @finish = finish
-    @visited = [start]
-    build_tree
-  end
+  # def knight_moves(start, finish)
+  #   @start = Knight.new(start)
+  #   @finish = finish
+  #   @visited = [start]
+  #   build_tree
+  # end
 
-  def build_tree(queue = [@start])
-    current = queue.last
-    return depth(current) if current.coord == @finish
+  # def build_tree(queue = [@start])
+  #   current = queue.last
+  #   return depth(current) if current.coord == @finish
 
-    current.possible_moves.each do |move|
-      next if @visited.include?(move)
+  #   current.possible_moves.each do |move|
+  #     next if @visited.include?(move)
 
-      knight_child = Knight.new(move)
-      current.children.push(knight_child)
-      knight_child.parent = current
-      @visited.push(knight_child.coord)
-      queue.unshift(knight_child)
-    end
+  #     knight_child = Knight.new(move)
+  #     current.children.push(knight_child)
+  #     knight_child.parent = current
+  #     @visited.push(knight_child.coord)
+  #     queue.unshift(knight_child)
+  #   end
 
-    queue.pop
-    build_tree(queue)
-  end
+  #   queue.pop
+  #   build_tree(queue)
+  # end
 
-  def depth(node, counter = 0, array = [])
-    array.unshift node.coord
-    return display_result(counter, array) if node == @start
+  # def depth(node, counter = 0, array = [])
+  #   array.unshift node.coord
+  #   return display_result(counter, array) if node == @start
 
-    depth(node.parent, counter + 1, array)
-  end
+  #   depth(node.parent, counter + 1, array)
+  # end
 
-  def display_result(counter, array)
-    puts "Your knight took #{counter} moves following this path: "
-    array.each { |coord| p coord }
-  end
+  # def display_result(counter, array)
+  #   puts "Your knight took #{counter} moves following this path: "
+  #   array.each { |coord| p coord }
+  # end
 end
