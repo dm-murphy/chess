1cefb3527f9b854ecd1887cbf649c829a62caf33
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-05-02 13:30:06 -0400

    Refactor Chess Spec tests to make all pass.

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -11,49 +11,49 @@ require './lib/king'
 require './lib/rook'
 require './lib/bishop'
 require './lib/queen'
-require './lib/castling.rb'
 require './lib/en_passant_moves.rb'
+require './lib/castling.rb'
+require './lib/pawn_promotion_moves.rb'
 
 describe Game do
+# describe 'start_turn' do
+# # Loop script sent to self
+# # Test methods in Board and MoveGenerator class
+# end
 
 # describe 'game_over?' do
 # # Script sent to self
-# # Tests and comments below for methods inside #checkmate?, #draw?
 # end
 
-# describe 'checkmate?' do
-# # Script sent to self, nested in #game_over?
-# # Tests and comments below for methods inside #all_legal_moves, #no_player_moves?, #king_in_check?
+# describe 'display_checkmate' do
+# # Puts message
 # end
 
-# describe 'all_legal_moves' do
-# # Query script sent to self, nested in #checkmate?
+# describe 'display_draw' do
+# # Puts message
 # end
+end
 
-# describe 'no_player_moves?' do
-# # Query sent to self, nested in #all_legal_moves -> #checkmate?
-# end
+describe MoveGenerator do
 
-# describe '#start_turn' do
-# # Loop script
-# # Tests and comments below for methods inside #display_user, #ask_user_start, #find_piece_moves, #find_piece_legal_moves, #ask_user_destination, #update_board, #game_over?, #swap_player
-# end
-
-# describe '#display_user' do
-# # Outgoing query sent to Board class and puts message, nested in #start_turn
-# end
+  subject(:test_game) { described_class.new(test_board, test_player_one, test_player_two) }
+  let(:test_board) { Board.new }
+  let(:test_player_one) { Player.new('Player 1', 'white') }
+  let(:test_player_two) { Player.new('Player 2', 'black') }
 
 # describe '#ask_user_start' do
-# # Loop script, nested in #start_turn
-# # Tests and comments below for methods inside #string_to_coord, #coords_to_grid_object, #player_piece?
+# # Loop script
+# # Tests and comments below for methods inside #ask_move, #string_to_coord, #coords_to_grid_object, #player_piece?
+# end
+
+# describe '#ask_move' do
+# # Outgoing query sent to Player class
 # end
 
   describe '#string_to_coord' do
-    # Query sent to self, found in #ask_user_start and #ask_user_destination
-    subject(:test_game) { described_class.new }
-  
+    # Query sent to self
     context 'when given a string of numbers' do
-  
+
       it 'returns array of coordinates' do
         string = '12'
         result = [1, 2]
@@ -62,22 +62,32 @@ describe Game do
     end
   end
 
+# describe 'no_player_moves?' do
+# # Query script sent to self
+# end
+
+# describe 'all_legal_moves' do
+########################
+######### TEST #########
+########################
+# # Command script sent to self
+# end
+
 # describe '#player_piece?' do
-# # Compares outgoing query to Player class and outgoing query to piece object's class
+# Query sent to self comparing outgoing query to Player class with outgoing query to piece's class
 # end
 
 # describe '#coords_to_grid_object' do
-# # Outgoing query sent to Board class, found in #check_piece, #start_turn and #occupied_by_player?
+# # Outgoing query sent to Board class
 # end
 
 # describe '#find_piece_moves' do
-# # Outgoing query sent to class of piece object, nested in #start_turn
+# # Outgoing query sent to piece's class
 # end
 
   describe '#find_piece_legal_moves' do
-    # Query sent to self, nested in #start_turn
-    subject(:test_game) { described_class.new }
-
+    # Query sent to self
+    
     context 'when all moves are legal' do
       it 'returns array with all moves' do
         moves = [[1, 3], [2, 2], [2, 0]]
@@ -122,13 +132,13 @@ describe Game do
   end
 
 # describe '#illegal_move?' do
-#  # Query script, nested in #find__piece_legal_moves -> #start_turn
-#  # Tests and comments below for methods inside #occupied_by_player?, #blocked?, #move_puts_self_in_check?, #king_stays_in_check?
+#  # Query script
+#  # Tests and comments below for methods inside #occupied_by_player?, #blocked?, #move_puts_self_in_check?,
+#  # #king_stays_in_check?, #illegal_pawn_move?
 # end
 
   describe '#occupied_by_player?' do
-    # Query outoing to player class and piece or square object, nested in #illegal_move? -> #find_piece_legal_moves -> #start_turn
-    subject(:test_game) { described_class.new }
+    # Query outgoing to player class and piece or square object
 
     context 'when coordinate is a blank square' do
 
@@ -192,31 +202,26 @@ describe Game do
   end
 
   describe 'king_in_check?' do
-    # Script, nested in #illegal_move? -> #find_piece_legal_moves -> #start_turn
-
-    subject(:test_game) { described_class.new }
+    # Query Script sent to self
 
     context 'when opponent Rook is at [7, 0] with opponent King at [0, 4] and player King at [7, 4] and player Knight at [7, 1]' do
 
       it 'returns false' do
 
-        move = [5, 5]
+        piece = King.new([7, 4], 'black')
         king_coord = [7, 4]
 
         test_moves = [[[7, 1], [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0]],
                      [[0, 3], [0, 5], [1, 3], [1, 4], [1, 5]]]
 
         allow(test_game).to receive(:find_opponent_moves) { test_moves }
-        expect(test_game.king_in_check?(move, king_coord)).to be false
+        expect(test_game.king_in_check?(piece, king_coord)).to be false
       end
     end
   end
 
   describe 'move_keeps_king_in_check?' do
-    # Query / Command to Board, nested in #illegal_move?
-
-    subject(:test_game) { described_class.new(test_board) }
-    let(:test_board){ Board.new }
+    # Query sent to self but uses commands in Board class
 
     context 'when opponent Rook is at [2, 4], and player King at [7, 4] with player move of Knight from [5, 2] to [6, 4]' do
 
@@ -233,31 +238,23 @@ describe Game do
   end
 
 # describe '#piece_is_king?' do
-#  # Query sent to self, nested in #king_in_check?
+#  # Query sent to self
 # end
 
 # describe '#find_king' do
-#  # Outgoing query sent to Board, called from #piece_is_king? and #checkmate?
-# end
-
-# describe '#find_opponent_moves' do
-#  # Script, nested in #king_in_check?
-#  # Tests below for methods inside #find_opponent, #find_pieces, #remove_possible_capture
-#  # For #find_possible_moves test the outgoing query #possible_moves in each piece class
+#  # Outgoing query sent to Board
 # end
 
   describe '#find_opponent_moves' do
 
-    subject(:test_game) { described_class.new }
-
     context 'when opponent Rook is at [7, 0] and opponent King at [0, 4] and player King at [7, 4] and player Knights at [7, 1] and [7, 6]' do
 
       it 'returns array of all opponent moves' do
+        opponent = 'white'
         move = [5, 5]
         result = [[[7, 1], [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0]],
                   [[0, 3], [0, 5], [1, 3], [1, 4], [1, 5]]]
 
-        test_opponent = 'white'
         test_rook = Rook.new([7, 7], 'white')
         test_king = King.new([0, 4], 'white')
         test_pieces = [test_rook, test_king]
@@ -265,56 +262,52 @@ describe Game do
         test_available_opponent_moves = [[[7, 1], [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0]],
         [[0, 3], [0, 5], [1, 3], [1, 4], [1, 5]]]
 
-        allow(test_game).to receive(:find_opponent) { test_opponent }
         allow(test_game).to receive(:find_pieces) { test_pieces }
         allow(test_game).to receive(:remove_possible_capture) { test_remaining_pieces }
         allow(test_game).to receive(:find_available_opponent_moves) { test_available_opponent_moves }
-        expect(test_game.find_opponent_moves(move)).to eq result
+        expect(test_game.find_opponent_moves(opponent, move)).to eq result
       end
     end
   end
 
   describe '#find_opponent' do
-    # Query sent to self, nested in #find_opponent_moves -> #king_in_check
-    
-    subject(:test_game) { described_class.new }
+    # Query sent to self
 
-    context "when @current_player pieces equals 'white'" do
-    
+    context "when piece equals 'white'" do
+
       it "returns 'black'" do
-        test_game.instance_variable_set(:@current_player, Player.new('Player 1', 'white'))
-
+        test_piece = Knight.new([0, 1], 'white')
         string = 'black'
-        expect(test_game.find_opponent).to eq string
+        expect(test_game.find_opponent(test_piece)).to eq string
       end
     end
-  
-    context "when @current_player pieces equals 'black'" do
-    
-      it "returns 'white'" do
-        test_game.instance_variable_set(:@current_player, Player.new('Player 2', 'black'))
 
+    context "when piece equals 'black'" do
+
+      it "returns 'white'" do
+        test_piece = Knight.new([7, 6], 'black')
         string = 'white'
-        expect(test_game.find_opponent).to eq string      
+        expect(test_game.find_opponent(test_piece)).to eq string
       end
     end 
   end
 
   describe '#find_pieces' do
-    # Query sent to self, nested in #find_opponent_moves -> #king_in_check?
+    # Query sent to self
 
-    let(:test_board) { Board.new }
-    subject(:test_game) { described_class.new(test_board) }
+    subject(:find_pieces_test_game) { described_class.new(pieces_test_board, pieces_test_player_one, pieces_test_player_two) }
+    let(:pieces_test_board) { Board.new }
+    let(:pieces_test_player_one) { Player.new('Player 1', 'white') }
+    let(:pieces_test_player_two) { Player.new('Player 2', 'black') }
     let(:opponent_knight_one) { Knight.new([7, 1], 'black') }
     let(:opponent_knight_two) { Knight.new([7, 6], 'black') }
     let(:opponent_king) { King.new([7, 4], 'black') }
 
+    context 'when mapping through black player pieces on Board grid' do
 
-    context 'when mapping through Board object grid' do
+      it 'returns array of pieces that match black player' do
 
-      it 'returns array of pieces that match opponent type' do
-
-        test_board.instance_variable_set(:@grid, 
+        pieces_test_board.instance_variable_set(:@grid,
           [
             [Square.new, Knight.new([0, 1], 'white'), Square.new, Square.new, King.new([0, 4], 'white'), Square.new, Knight.new([0, 6], 'white'), Square.new],
             [Square.new, Square.new, Square.new, Square.new, Square.new, Square.new, Square.new, Square.new],
@@ -324,27 +317,25 @@ describe Game do
             [Square.new, Square.new, Square.new, Square.new, Square.new, Square.new, Square.new, Square.new],
             [Square.new, Square.new, Square.new, Square.new, Square.new, Square.new, Square.new, Square.new],
             [Square.new, opponent_knight_one, Square.new, Square.new, opponent_king, Square.new, opponent_knight_two, Square.new]
-          ]  
+          ]
         )
 
-        opponent = 'black'
-
-        opponent_pieces = [opponent_knight_one, opponent_king, opponent_knight_two]
-        expect(test_game.find_pieces(opponent)).to eq opponent_pieces
+        test_player = pieces_test_player_two.pieces
+        test_pieces = [opponent_knight_one, opponent_king, opponent_knight_two]
+        expect(find_pieces_test_game.find_pieces(test_player)).to eq test_pieces
       end
     end
   end
 
   describe '#remove_possible_capture' do
-    # Query sent to self, nested in #find_opponent_moves -> #king_in_check?
+    # Query sent to self
 
-    subject(:test_game) { described_class.new }
     let(:opponent_knight_one) { Knight.new([2, 1], 'black') }
     let(:opponent_knight_two) { Knight.new([3, 4], 'black') }
     let(:opponent_king) { King.new([7, 4], 'black') }
 
     context 'when opponent pieces includes a coordinate the current player is moving to capture' do
-      
+
       it 'returns array of opponent pieces without the captured piece' do
         opponent_pieces = [opponent_knight_one, opponent_knight_two, opponent_king]
         move = [2, 1]
@@ -366,16 +357,8 @@ describe Game do
     end
   end
 
-# describe '#find_all_pieces_possible_moves' do
-  # Query sent to each piece objects class: (Knight, King), nested in #find_opponent_moves -> #king_in_check
-# end
-
   describe '#coord_in_check?' do
-    # Query sent to self, nested in #king_in_check? -> #illegal_move? -> #find_piece_legal_moves -> #start_turn
-
-    # Opponent_moves show all opponent piece moves
-
-    subject(:test_game) { described_class.new } 
+    # Query sent to self
   
     context 'when opponent Knight at [1, 2] and player King at [0 , 4]' do
   
@@ -398,7 +381,6 @@ describe Game do
     context 'when opponent Rook is at [7, 0] with opponent King at [0, 4] and player King at [7, 4] and player Knight at [7, 1]' do
     
       it 'returns false' do
-        # opponent_moves = [[6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0]]
         opponent_moves = [[[7, 1], [6, 0], [5, 0], [4, 0], [3, 0], [2, 0], [1, 0], [0, 0]],
                           [[0, 3], [0, 5], [1, 3], [1, 4], [1, 5]]]
         coord = [7, 4]
@@ -408,19 +390,16 @@ describe Game do
   end
 
 # describe '#ask_user_destination' do
-  # Query loop sent to Player class, returns user input as coordinate, nested in nested in #start_turn
+  # Query loop sent to Player class, returns user input as coordinate
 # end
 
   describe '#update_board' do
     # Outgoing Command, test message sent
 
-    let(:board) { instance_double(Board) }
-    subject(:test_game) { described_class.new(board) }
-
     context 'when user makes a legal move' do
 
       it 'updates the board' do
-        expect(board).to receive(:change_pieces)
+        expect(test_board).to receive(:change_pieces)
         start_coord = [0, 1]
         destination_coord = [2, 2]
         test_game.update_board(start_coord, destination_coord)
@@ -429,9 +408,7 @@ describe Game do
   end
 
   describe '#swap_player' do
-    # Command sent to self, but #swap_player is part of public script in #start so test
-
-    subject(:test_game) { described_class.new }
+    # Command sent to self
 
     context 'when @current_player is @player_one' do
 
