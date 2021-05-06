b49edfb7d8969feeb9f2b29258c0ce675de3f574
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-05-06 12:44:53 -0400

    Add spec file for Rook class but no tests needed

---
+++ spec/rook_spec.rb
@@ -0,0 +1,28 @@
+#spec/rook_spec.rb
+
+require './lib/game'
+require './lib/move_generator.rb'
+require './lib/player'
+require './lib/board'
+require './lib/square'
+require './lib/piece'
+require './lib/pawn'
+require './lib/knight'
+require './lib/king'
+require './lib/rook'
+require './lib/bishop'
+require './lib/queen'
+require './lib/en_passant_moves.rb'
+require './lib/castling.rb'
+require './lib/pawn_promotion_moves.rb'
+
+describe Rook do
+
+  # describe '#coord_changes'
+  # # Query sent to self
+  # end
+
+  # describe '#next_space_moves'
+  # # Query send to self
+  # end
+end
